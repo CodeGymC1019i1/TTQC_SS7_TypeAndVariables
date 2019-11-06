0d33aa3ff695d4be9d7f536421bdc969576805d7
CuongVM <vozerkrp@users.noreply.github.com>
2019-11-06 08:17:54 +0700

    nop bt

---
+++ vu minh cuong/js/1.js
@@ -0,0 +1,9 @@
+var inputWidth;
+var inputHeight;
+inputWidth = prompt("nhập chiều rộng:");
+inputHeight = prompt("nhập chiều cao:");
+var width = parseInt(inputWidth);
+var height = parseInt(inputHeight);
+var area = width * height;
+
+document.write("The area is: " + area);
